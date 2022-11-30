73158dfae30701194401ca83af491de96485a4c8
Javier Ignacio Rivarola <105676056+javotechno@users.noreply.github.com>
2022-11-29 22:45:00 -0300

    inscripción de FireBase .env
    Comandos de inscripción.

--- config.js
+++ config.js
@@ -10,3 +10,12 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
+
+//incripción de Firebass, - .env -
+
+//REACT_APP_API_KEY=AIzaSyAK93MCQ5lRsDvWmzZzYH-CpgKcKSj-VEk
+//REACT_APP_AUTH_DOMAIND=alaska-d8049.firebaseapp.com
+//REACT_APP_PROJECT_ID=alaska-d8049
+//REACT_APP_STORAGE_BUCKET=alaska-d8049.appspot.com
+//REACT_APP_MESSAGING_SENDER_ID=472857591982
+//REACT_APP_APP_ID=1:472857591982:web:e02faad944a08c9d99ad3f
